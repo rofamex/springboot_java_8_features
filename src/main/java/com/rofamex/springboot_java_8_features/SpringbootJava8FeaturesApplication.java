de3b19b6fe7694b0470fbb07cd17716c4e22ab40
Robson Lee <rofamex@gmail.com>
2021-04-14 12:16:18 -0400

    springboot initial commit

---
+++ src/main/java/com/rofamex/springboot_java_8_features/SpringbootJava8FeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.rofamex.springboot_java_8_features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJava8FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJava8FeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rofamex/springboot_java_8_features/SpringbootJava8FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rofamex.springboot_java_8_features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJava8FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
