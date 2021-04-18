539d6a324f0c8a0ee5da8b223d2d32f19995eed2
Robson Lee <rofamex@gmail.com>
2021-04-17 23:38:31 -0400

    refactoring

--- src/main/java/com/rofamex/springboot_java_8_features/repository/CustomerRepository.java
+++ src/main/java/com/rofamex/springboot_java_8_features/repository/CustomerRepository.java
@@ -1,9 +1,6 @@
 package com.rofamex.springboot_java_8_features.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rofamex.springboot_java_8_features.entity.Customer;
